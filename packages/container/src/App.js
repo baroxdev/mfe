bbf79b269a474b72c5d313dda952f13bbed250fc
baroxdev <barox.dev@gmail.com>
2026-03-29 12:00:49 +0700

    feat(container): update App component structure and enhance AWS deployment workflow

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { mount } from "marketing/Marketing";
 import MarketingApp from "./components/MarketingApp";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default function App() {
   return (
-    <div>
-      <h1>Container</h1>
-      <MarketingApp />
-    </div>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 }
