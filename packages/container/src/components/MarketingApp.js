91b6a941f8a89abec903428b53dbfa29ebfab038
baroxdev <barox.dev@gmail.com>
2026-03-31 17:40:39 +0700

    feat(marketing): enhance routing by integrating history management in MarketingApp and App components

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,12 +1,22 @@
 import React, { useRef, useEffect } from "react";
 import { mount } from "marketing/Marketing";
-
+import { useHistory } from "react-router-dom";
 export default function MarketingApp() {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
-  }, []);
+    const { onParentNavigate } = mount(ref.current, {
+      onCallback: ({ pathname: nextPathname }) => {
+        if (history.location.pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+      defaultPathname: history.location.pathname,
+    });
+
+    history.listen(onParentNavigate);
+  }, [history]);
 
   return <div ref={ref} />;
 }
--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,15 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { createMemoryHistory, createBrowserHistory } from "history";
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onCallback, defaultHistory, defaultPathname }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onCallback) {
+    history.listen(onCallback);
+  }
+
+  if (defaultPathname) {
+    history.push(defaultPathname);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      console.log({ nextPathname });
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
 };
 
 if (process.env.NODE_ENV === "development") {
   const devRoot = document.querySelector("#_marketing-dev-root");
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
